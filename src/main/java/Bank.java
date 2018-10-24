075d2a5bb816dd879f3d71c4cc43bd0605609ac4
andredur <andreasdurt@hotmail.com>
2018-10-24 11:13:02 +0200

    interface

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -1,2 +1,8 @@
-public interface Bank {
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface Bank extends Remote {
+    String balance() throws RemoteException;
+    String addMoney() throws RemoteException;
+    String withdrawMoney() throws RemoteException;
 }
--- src/main/java/BankServer.java
+++ src/main/java/BankServer.java
@@ -1,2 +1,32 @@
-public class BankServer {
+import java.rmi.registry.Registry;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.server.UnicastRemoteObject;
+
+public class BankServer implements Bank {
+
+    public BankServer() {}
+
+    public String balance() {
+        return "Hello, world!";
+    }
+
+    public static void main(String args[]) {
+
+        try {
+            Server obj = new Server();
+            Hello stub = (Hello) UnicastRemoteObject.exportObject(obj, 0);
+            LocateRegistry.createRegistry(1099);
+            // Bind the remote object's stub in the registry
+            //System.setProperty("java.rmi.server.hostname","192.168.1.1");
+            //Registry registry = LocateRegistry.getRegistry();
+            Registry registry = LocateRegistry.getRegistry();
+            registry.bind("Hello", stub);
+
+            System.err.println("Server ready");
+        } catch (Exception e) {
+            System.err.println("Server exception: " + e.toString());
+            e.printStackTrace();
+        }
+    }
 }
+
